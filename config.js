e9b600051855a473f8d956e5d8ce3f961baf09a2
Virk <40689073+virkx3@users.noreply.github.com>
2025-06-23 15:18:37 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    SMMWIZ_API_KEY: process.env.SMMWIZ_API_KEY || 'ccdc7bc9cda18e72b22ed9ba9d13c872',
+    SERVICE_ID: process.env.SERVICE_ID || '13826',
+    USERNAME: process.env.USERNAME || 'iamvirk05',
+    QUANTITY: process.env.QUANTITY || 20
+};
